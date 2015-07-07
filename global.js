9f712b4b8fdb8b5de59c5e08b939fddaf446d540
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-07 12:14:48 -0500

    Added verification for answer and score update.
    Added for loop for questions array.

--- global.js
+++ global.js
@@ -1,21 +1,36 @@
 var question1 = {
   question: "A magical jar of jelly beans doubles the number of jelly beans every minute.  The jar is full at noon.  When is it half full?",
   answer: "11:59",
-  arrayOfPossibleAnswers: ["11:59", "11:30", "It is impossible to answer this question.", "12:01"]
+  possibleAnswers: ["11:59", "11:30", "It is impossible to answer this question.", "12:01"]
 };
 
-var directions = "Enter the number corresponding to your answer.";
+var questions = [question1];
+var score = 0;
 
-var wholeQuestion = directions + "\n\n" + question1.question + "\n";
+for (i = 0; i < questions.length; i++){
+  
+  var directions = "Enter the number corresponding to your answer.";
 
-for (count = 0; count < question1.arrayOfPossibleAnswers.length; count++ ){
-  wholeQuestion += "\n " + (count + 1) + ". " + question1.arrayOfPossibleAnswers[count];
-}
+  var wholeQuestion = directions + "\n\n" + questions[i].question + "\n";
 
-var answer = parseInt(prompt(wholeQuestion));
+  for (count = 0; count < questions[i].possibleAnswers.length; count++ ){
+    wholeQuestion += "\n " + (count + 1) + ". " + questions[i].possibleAnswers[count];
+  }
 
-while (answer < 1 || answer > question1.arrayOfPossibleAnswers.length || isNaN(answer)) {
-  alert("That is not a valid answer.");
   var answer = parseInt(prompt(wholeQuestion));
-} 
+
+  while (answer < 1 || answer > questions[i].possibleAnswers.length || isNaN(answer)){
+    alert("That is not a valid answer.");
+    var answer = parseInt(prompt(wholeQuestion));
+  } 
+  
+  if (questions[i].possibleAnswers[answer-1] === questions[i].answer){
+    alert("Congratulations! You picked the correct answer.");
+    score++;
+  }
+  else{
+    alert("Wrong answer.")
+  }
+}
+  
   
